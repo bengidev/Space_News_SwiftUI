e2cc20c37fa097069f7ef3b6abe53751ea98e873
B e n g i <bambang.trd17@gmail.com>
2024-02-08 15:03:47 +0700

    chore: initialize base project files from XCode

---
+++ Space News/Space News/Space_NewsApp.swift
@@ -0,0 +1,20 @@
+//
+//  Space_NewsApp.swift
+//  Space News
+//
+//  Created by Bambang Tri Rahmat Doni on 08/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Space_NewsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
