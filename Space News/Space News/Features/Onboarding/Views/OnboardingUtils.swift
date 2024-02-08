3ec43d206487d75b3ac4560aed8d4953716c8163
B e n g i <bambang.trd17@gmail.com>
2024-02-08 17:58:17 +0700

    chore: add OnboardingUtils into Onboarding

---
+++ Space News/Space News/Features/Onboarding/Views/OnboardingUtils.swift
@@ -0,0 +1,31 @@
+//
+//  OnboardingUtils.swift
+//  Space News
+//
+//  Created by Bambang Tri Rahmat Doni on 08/02/24.
+//
+
+import Foundation
+
+enum OnboardingUtils {
+  static let title: String = "Welcome to Space News!"
+  static let subTitle: String =
+    "Explore the wonders of the universe with our cutting-edge Space News app." +
+    " Stay informed about the latest discoveries, missions, and astronomical events from around the cosmos."
+  static let featureOne: String = "Personalized News Feed"
+  static let subFeatureOne: String =
+    "Tailor your space journey by selecting your favorite topics." +
+    " Whether it's black holes, exoplanets, or space exploration, we've got you covered."
+  static let featureTwo: String = "Real-time Updates"
+  static let subFeatureTwo: String =
+    "Receive instant notifications about breaking news and major space events. " +
+    "Be the first to know about exciting developments in the world beyond our atmosphere."
+  static let featureThree: String = "In-depth Articles"
+  static let subFeatureThree: String =
+    "Delve into detailed articles written by experts in the field. " +
+    "Gain a deeper understanding of complex astronomical phenomena and missions."
+  static let featureFour: String = "Save and Share"
+  static let subFeatureFour: String =
+    "Bookmark your favorite articles and share them with fellow space enthusiasts. " +
+    "Spark conversations and inspire others with the wonders of the cosmos."
+}
