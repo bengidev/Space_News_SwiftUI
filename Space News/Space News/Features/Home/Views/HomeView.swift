0de900fb4ed3a1e3a795151d937d02937b00fe77
B e n g i <bambang.trd17@gmail.com>
2024-02-13 17:40:09 +0700

    chore: add NewsCategory inside HomeView

--- Space News/Space News/Features/Home/Views/HomeView.swift
+++ Space News/Space News/Features/Home/Views/HomeView.swift
@@ -9,13 +9,57 @@ import Inject
 import SwiftUI
 
 struct HomeView: View {
-  var text: String?
+  @Environment(\.colorScheme) private var colorScheme
+
+  @State private var searchText: String = ""
+  @State private var tabs: [String] = ["Nature", "Animals", "Fish", "Flowers", "Cities", "Cars", "Planes"]
+  @State private var selectedTab: Int = 0
 
   @ObservedObject private var injectObserver = Inject.observer
 
   var body: some View {
     VStack {
-      Text(self.text ?? "Hello, World!")
+
+      HStack(spacing: 0.0) {
+        Text("Hello, Good Morning")
+          .font(.system(.headline, design: .rounded))
+
+        Spacer()
+
+        Button("", systemImage: "moon.fill", action: {})
+          .tint(self.colorScheme == .dark ? Color.white : Color.black)
+          .labelsHidden()
+
+      }
+      .padding(.horizontal, 5.0)
+
+      HStack(spacing: 0.0) {
+        Image(systemName: "magnifyingglass")
+
+        Spacer()
+
+        TextField("Find interesting news", text: self.$searchText)
+      }
+      .padding(10.0)
+      .overlay(
+        RoundedRectangle(cornerRadius: 10.0)
+          .stroke(Color.gray, lineWidth: 1.0)
+      )
+      .padding(.horizontal, 5.0)
+
+      ScrollView(.horizontal, showsIndicators: false) {
+        HStack(spacing: 30.0) {
+          ForEach(0 ..< self.tabs.count, id: \.self) { tab in
+            NewsCategory(menus: self.tabs, menuIndex: tab, selectedIndex: self.$selectedTab) {
+              withAnimation {
+                self.selectedTab = tab
+              }
+            }
+          }
+        }
+        .padding(.horizontal)
+      }
+      .padding(.top, 10.0)
     }
     .enableInjection()
   }
