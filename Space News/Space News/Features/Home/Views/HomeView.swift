47ec738996c491649ce7643312046511ba796abd
B e n g i <bambang.trd17@gmail.com>
2024-02-14 18:50:58 +0700

    fix: rearrange CustomCarousel with ScrollView

--- Space News/Space News/Features/Home/Views/HomeView.swift
+++ Space News/Space News/Features/Home/Views/HomeView.swift
@@ -15,66 +15,95 @@ struct HomeView: View {
   @State private var tabs: [String] = ["Nature", "Animals", "Fish", "Flowers", "Cities", "Cars", "Planes"]
   @State private var selectedTab: Int = 0
   @State private var selectedCarousel: Int = 0
+  @State private var showDetailView: Bool = false
+  @State private var currentDetailNews: String = ""
+
+  @Namespace private var animation
 
   @ObservedObject private var injectObserver = Inject.observer
 
   var body: some View {
-    VStack {
+    NavigationView {
+      ScrollView(.vertical, showsIndicators: false) {
+        VStack {
+          HStack(spacing: 0.0) {
+            Text("Hello, Good Morning")
+              .font(.system(.headline, design: .rounded))
 
-      HStack(spacing: 0.0) {
-        Text("Hello, Good Morning")
-          .font(.system(.headline, design: .rounded))
+            Spacer()
 
-        Spacer()
+            Button("", systemImage: "moon.fill", action: {})
+              .tint(self.colorScheme == .dark ? Color.white : Color.black)
+              .labelsHidden()
+          }
+          .padding(.horizontal, 5.0)
 
-        Button("", systemImage: "moon.fill", action: {})
-          .tint(self.colorScheme == .dark ? Color.white : Color.black)
-          .labelsHidden()
+          HStack(spacing: 0.0) {
+            Image(systemName: "magnifyingglass")
 
-      }
-      .padding(.horizontal, 5.0)
+            Spacer()
 
-      HStack(spacing: 0.0) {
-        Image(systemName: "magnifyingglass")
+            TextField("Find interesting news", text: self.$searchText)
+          }
+          .padding(10.0)
+          .overlay(
+            RoundedRectangle(cornerRadius: 10.0)
+              .stroke(Color.gray, lineWidth: 1.0)
+          )
+          .padding(.horizontal, 5.0)
 
-        Spacer()
-
-        TextField("Find interesting news", text: self.$searchText)
-      }
-      .padding(10.0)
-      .overlay(
-        RoundedRectangle(cornerRadius: 10.0)
-          .stroke(Color.gray, lineWidth: 1.0)
-      )
-      .padding(.horizontal, 5.0)
-
-      ScrollView(.horizontal, showsIndicators: false) {
-        HStack(spacing: 30.0) {
-          ForEach(0 ..< self.tabs.count, id: \.self) { tab in
-            NewsCategory(menus: self.tabs, menuIndex: tab, selectedIndex: self.$selectedTab) {
-              withAnimation {
-                self.selectedTab = tab
+          ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 30.0) {
+              ForEach(0 ..< self.tabs.count, id: \.self) { tab in
+                NewsCategory(menus: self.tabs, menuIndex: tab, selectedIndex: self.$selectedTab) {
+                  withAnimation {
+                    self.selectedTab = tab
+                  }
+                }
               }
             }
+            .padding(.horizontal)
           }
-        }
-        .padding(.horizontal)
-      }
-      .padding(.top, 10.0)
+          .padding(.top, 10.0)
 
-      CustomCarousel(
-        index: self.$selectedCarousel,
-        items: self.tabs,
-        spacing: 10.0,
-        cardPadding: 90.0,
-        id: \.self
-      ) { _, _ in
-        ZStack {
-          RoundedRectangle(cornerRadius: 30.0, style: .continuous)
+          Text("Highlights")
+            .font(.system(.subheadline, design: .rounded).bold())
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal, 5.0)
+            .padding(.vertical, 10.0)
+
+          CustomCarousel(
+            index: self.$selectedCarousel,
+            items: self.tabs,
+            spacing: 10.0,
+            cardPadding: 90.0,
+            id: \.self
+          ) { carousel, _ in
+            ZStack {
+              RoundedRectangle(cornerRadius: 30.0, style: .continuous)
+                .foregroundStyle(Color.gray)
+              Text(carousel)
+            }
+            .padding(.horizontal, 10.0)
+            .contentShape(Rectangle())
+            .onTapGesture {
+              hideTabBar()
+              withAnimation(.interactiveSpring(response: 0.5, dampingFraction: 0.7, blendDuration: 0.7)) {
+                self.currentDetailNews = carousel
+                self.showDetailView = true
+              }
+
+            }
+          }
+          .frame(height: 250.0)
         }
-        .frame(height: 380.0)
-        .padding(.horizontal, 10.0)
+        NavigationLink(
+          destination: DetailView(),
+          isActive: self.$showDetailView,
+          label: {}
+        )
       }
+      .background(Color.appSecondary)
     }
     .enableInjection()
   }
