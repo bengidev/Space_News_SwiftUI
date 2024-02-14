b52ae4ae78116e4d2101120fc28c90adab857a4e
B e n g i <bambang.trd17@gmail.com>
2024-02-14 18:54:23 +0700

    feat: show or hide TabBar with conditionals

--- Space News/Space News/Features/Dashboard/Views/DashboardView.swift
+++ Space News/Space News/Features/Dashboard/Views/DashboardView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct DashboardView: View {
   @State private var selectedTabItem: AppTabCategory = .home
+  @State private var isShowingTabBar: Bool = true
 
   @ObservedObject private var injectObserver = Inject.observer
 
@@ -71,9 +72,31 @@ struct DashboardView: View {
       }
       .padding(.horizontal)
       .background(Color.clear)
+      .offset(y: self.isShowingTabBar ? 0 : 150.0)
+      .animation(
+        .interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7),
+        value: self.isShowingTabBar
+      )
     }
     .ignoresSafeArea(.keyboard, edges: .bottom)
+    .onReceive(NotificationCenter.default.publisher(for: .init("SHOW_TAB_BAR"))) { _ in
+      self.isShowingTabBar = true
+    }
+    .onReceive(NotificationCenter.default.publisher(for: .init("HIDE_TAB_BAR"))) { _ in
+      self.isShowingTabBar = false
+    }
+    .background(Color.appSecondary)
     .enableInjection()
+  }
+}
+
+extension View {
+  func showTabBar() {
+    NotificationCenter.default.post(name: .init("SHOW_TAB_BAR"), object: nil, userInfo: nil)
+  }
+
+  func hideTabBar() {
+    NotificationCenter.default.post(name: .init("HIDE_TAB_BAR"), object: nil, userInfo: nil)
   }
 }
 
