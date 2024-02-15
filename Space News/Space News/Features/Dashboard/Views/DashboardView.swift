f730b77eb7f98f304ecb2354a3792810db9816d6
B e n g i <bambang.trd17@gmail.com>
2024-02-15 18:34:15 +0700

    style: change TabBar styles inside Dashboard

--- Space News/Space News/Features/Dashboard/Views/DashboardView.swift
+++ Space News/Space News/Features/Dashboard/Views/DashboardView.swift
@@ -9,97 +9,83 @@ import Inject
 import SwiftUI
 
 struct DashboardView: View {
-  @State private var selectedTabItem: AppTabCategory = .home
+  @State private var selectedTabItem: AppTabViewCategory = .home
   @State private var isShowingTabBar: Bool = true
+
+  @Namespace private var animation
 
   @ObservedObject private var injectObserver = Inject.observer
 
+
+        Spacer(minLength: 0)
+
   var body: some View {
-    VStack(spacing: 0) {
-      TabView(selection: self.$selectedTabItem) {
-        HomeView()
-          .tag(AppTabCategory.home)
-        ExploreView()
-          .tag(AppTabCategory.explore)
-        FavoritesView()
-          .tag(AppTabCategory.favorites)
-        AccountView()
-          .tag(AppTabCategory.account)
+    NavigationView {
+      ZStack(alignment: .bottom) {
+        TabView(selection: self.$selectedTabItem) {
+          HomeView()
+            .tag(AppTabViewCategory.home)
+          ExploreView()
+            .tag(AppTabViewCategory.explore)
+          FavoritesView()
+            .tag(AppTabViewCategory.favorites)
+          AccountView()
+            .tag(AppTabViewCategory.account)
+        }
+
+        HStack(spacing: 0) {
+          ForEach(AppTabViewCategory.allCases, id: \.rawValue) { tab in
+            Button {
+              self.selectedTabItem = tab
+            } label: {
+              Image(systemName: "star.fill")
+                .resizable()
+                .renderingMode(.template)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 20.0, height: 20.0)
+                .foregroundStyle(self.selectedTabItem == tab ? Color.white : Color.black)
+                .offset(y: self.selectedTabItem == tab ? -30.0 : 0)
+                .background {
+                  if self.selectedTabItem == tab {
+                    Circle()
+                      .fill(Color.black)
+                      .scaleEffect(2.5)
+                      .shadow(color: Color.black.opacity(0.3), radius: 8.0, x: 5.0, y: 10.0)
+                      .matchedGeometryEffect(id: "DashboardView", in: self.animation)
+                      .offset(y: self.selectedTabItem == tab ? -30.0 : 0)
+                  }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.top, 15.0)
+                .padding(.bottom, 10.0)
+                .contentShape(Rectangle())
+            }
+          }
+        }
+        .padding(.horizontal)
+        .offset(y: self.isShowingTabBar ? 0 : 150.0)
+        .animation(
+          .interactiveSpring(response: 0.5, dampingFraction: 0.65, blendDuration: 0.65),
+          value: self.selectedTabItem
+        )
+        .animation(
+          .interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7),
+          value: self.isShowingTabBar
+        )
+        .background {
+          AppTabViewCorner(corners: [.topLeft, .topRight], radius: 25.0)
+            .fill(Color.gray)
+            .ignoresSafeArea()
+        }
       }
-
-      HStack(spacing: 0) {
-        AppTabViewItem(
-          tabItemName: "Home",
-          tabItemImage: "house.fill",
-          selectedTabItem: self.selectedTabItem,
-          tabItemTag: .home
-        ) { tabCategory in
-          withAnimation { self.selectedTabItem = tabCategory }
-        }
-
-        Spacer(minLength: 0)
-
-        AppTabViewItem(
-          tabItemName: "Explore",
-          tabItemImage: "map.fill",
-          selectedTabItem: self.selectedTabItem,
-          tabItemTag: .explore
-        ) { tabCategory in
-          withAnimation { self.selectedTabItem = tabCategory }
-        }
-
-        Spacer(minLength: 0)
-
-        AppTabViewItem(
-          tabItemName: "Favorites",
-          tabItemImage: "star.fill",
-          selectedTabItem: self.selectedTabItem,
-          tabItemTag: .favorites
-        ) { tabCategory in
-          withAnimation { self.selectedTabItem = tabCategory }
-        }
-
-        Spacer(minLength: 0)
-
-        AppTabViewItem(
-          tabItemName: "Account",
-          tabItemImage: "person.fill",
-          selectedTabItem: self.selectedTabItem,
-          tabItemTag: .account
-        ) { tabCategory in
-          withAnimation { self.selectedTabItem = tabCategory }
-        }
+      .ignoresSafeArea(.keyboard, edges: .bottom)
+      .onReceive(NotificationCenter.default.publisher(for: .init("SHOW_TAB_BAR"))) { _ in
+        self.isShowingTabBar = true
       }
-      .padding(.horizontal)
-      .background(Color.clear)
-      .offset(y: self.isShowingTabBar ? 0 : 150.0)
-      .animation(
-        .interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7),
-        value: self.isShowingTabBar
-      )
+      .onReceive(NotificationCenter.default.publisher(for: .init("HIDE_TAB_BAR"))) { _ in
+        self.isShowingTabBar = false
+      }
+      .enableInjection()
     }
-    .ignoresSafeArea(.keyboard, edges: .bottom)
-    .onReceive(NotificationCenter.default.publisher(for: .init("SHOW_TAB_BAR"))) { _ in
-      self.isShowingTabBar = true
-    }
-    .onReceive(NotificationCenter.default.publisher(for: .init("HIDE_TAB_BAR"))) { _ in
-      self.isShowingTabBar = false
-    }
-    .background(Color.appSecondary)
-    .enableInjection()
   }
-}
-
-extension View {
-  func showTabBar() {
-    NotificationCenter.default.post(name: .init("SHOW_TAB_BAR"), object: nil, userInfo: nil)
-  }
-
-  func hideTabBar() {
-    NotificationCenter.default.post(name: .init("HIDE_TAB_BAR"), object: nil, userInfo: nil)
-  }
-}
-
-#Preview {
-  DashboardView()
 }
