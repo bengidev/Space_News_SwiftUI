cd9dea4135c6012fbe1b814ed300b9e701ac9384
B e n g i <bambang.trd17@gmail.com>
2024-02-15 18:31:10 +0700

    chore: add AppTabViewCategory for separation

---
+++ Space News/Space News/Utilities/AppTabViewCategory.swift
@@ -0,0 +1,15 @@
+//
+//  AppTabViewCategory.swift
+//  Space News
+//
+//  Created by Bambang Tri Rahmat Doni on 15/02/24.
+//
+
+import Foundation
+
+enum AppTabViewCategory: String, CaseIterable {
+  case home
+  case explore
+  case favorites
+  case account
+}
