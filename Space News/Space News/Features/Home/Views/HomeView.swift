99cc75c3a2cd2e41ac3a713b3e1c6ee8783206b4
B e n g i <bambang.trd17@gmail.com>
2024-02-15 18:35:55 +0700

    fix: move NavigationView to parent DashboardView

--- Space News/Space News/Features/Home/Views/HomeView.swift
+++ Space News/Space News/Features/Home/Views/HomeView.swift
@@ -23,92 +23,85 @@ struct HomeView: View {
   @ObservedObject private var injectObserver = Inject.observer
 
   var body: some View {
-    NavigationView {
-      ScrollView(.vertical, showsIndicators: false) {
-        VStack {
-          HStack(spacing: 0.0) {
-            Text("Hello, Good Morning")
-              .font(.system(.headline, design: .rounded))
+    ScrollView(.vertical, showsIndicators: false) {
+      VStack {
+        HStack(spacing: 0.0) {
+          Text("Hello, Good Morning")
+            .font(.system(.headline, design: .rounded))
 
-            Spacer()
+          Spacer()
 
-            Button("", systemImage: "moon.fill", action: {})
-              .tint(self.colorScheme == .dark ? Color.white : Color.black)
-              .labelsHidden()
-          }
-          .padding(.horizontal, 5.0)
+          Button("", systemImage: "moon.fill", action: {})
+            .tint(self.colorScheme == .dark ? Color.white : Color.black)
+            .labelsHidden()
+        }
+        .padding(.horizontal, 5.0)
 
-          HStack(spacing: 0.0) {
-            Image(systemName: "magnifyingglass")
+        HStack(spacing: 0.0) {
+          Image(systemName: "magnifyingglass")
 
-            Spacer()
+          Spacer()
 
-            TextField("Find interesting news", text: self.$searchText)
-          }
-          .padding(10.0)
-          .overlay(
-            RoundedRectangle(cornerRadius: 10.0)
-              .stroke(Color.gray, lineWidth: 1.0)
-          )
-          .padding(.horizontal, 5.0)
+          TextField("Find interesting news", text: self.$searchText)
+        }
+        .padding(10.0)
+        .overlay(
+          RoundedRectangle(cornerRadius: 10.0)
+            .stroke(Color.gray, lineWidth: 1.0)
+        )
+        .padding(.horizontal, 5.0)
 
-          ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 30.0) {
-              ForEach(0 ..< self.tabs.count, id: \.self) { tab in
-                NewsCategory(menus: self.tabs, menuIndex: tab, selectedIndex: self.$selectedTab) {
-                  withAnimation {
-                    self.selectedTab = tab
-                  }
+        ScrollView(.horizontal, showsIndicators: false) {
+          HStack(spacing: 30.0) {
+            ForEach(0 ..< self.tabs.count, id: \.self) { tab in
+              NewsCategory(menus: self.tabs, menuIndex: tab, selectedIndex: self.$selectedTab) {
+                withAnimation {
+                  self.selectedTab = tab
                 }
               }
             }
-            .padding(.horizontal)
           }
-          .padding(.top, 10.0)
-
-          Text("Highlights")
-            .font(.system(.subheadline, design: .rounded).bold())
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.horizontal, 5.0)
-            .padding(.vertical, 10.0)
-
-          CustomCarousel(
-            index: self.$selectedCarousel,
-            items: self.tabs,
-            spacing: 10.0,
-            cardPadding: 90.0,
-            id: \.self
-          ) { carousel, _ in
-            ZStack {
-              RoundedRectangle(cornerRadius: 30.0, style: .continuous)
-                .foregroundStyle(Color.gray)
-              Text(carousel)
-            }
-            .padding(.horizontal, 10.0)
-            .contentShape(Rectangle())
-            .onTapGesture {
-              hideTabBar()
-              withAnimation(.interactiveSpring(response: 0.5, dampingFraction: 0.7, blendDuration: 0.7)) {
-                self.currentDetailNews = carousel
-                self.showDetailView = true
-              }
-
-            }
-          }
-          .frame(height: 250.0)
+          .padding(.horizontal)
         }
-        NavigationLink(
-          destination: DetailView(),
-          isActive: self.$showDetailView,
-          label: {}
-        )
+        .padding(.top, 10.0)
+
+        Text("Highlights")
+          .font(.system(.subheadline, design: .rounded).bold())
+          .frame(maxWidth: .infinity, alignment: .leading)
+          .padding(.horizontal, 5.0)
+          .padding(.vertical, 10.0)
+
+        AppCardCarousel(
+          index: self.$selectedCarousel,
+          items: self.tabs,
+          spacing: 10.0,
+          cardPadding: 90.0,
+          id: \.self
+        ) { carousel, _ in
+          ZStack {
+            RoundedRectangle(cornerRadius: 30.0, style: .continuous)
+              .foregroundStyle(Color.gray)
+            Text(carousel)
+          }
+          .padding(.horizontal, 10.0)
+          .contentShape(Rectangle())
+          .onTapGesture {
+            withAnimation(.interactiveSpring(response: 0.5, dampingFraction: 0.7, blendDuration: 0.7)) {
+              self.currentDetailNews = carousel
+              self.showDetailView = true
+            }
+
+          }
+        }
+        .frame(height: 250.0)
       }
-      .background(Color.appSecondary)
+      NavigationLink(
+        destination: DetailView(),
+        isActive: self.$showDetailView,
+        label: {}
+      )
     }
+    .background(Color.appSecondary)
     .enableInjection()
   }
-}
-
-#Preview {
-  HomeView()
 }
