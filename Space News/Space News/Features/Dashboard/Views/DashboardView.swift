4a53427fd7523e6c54112d83b5857b9cf0bcdc77
B e n g i <bambang.trd17@gmail.com>
2024-02-17 17:18:27 +0700

    refactor: use relevant parameters to avoid errors

--- Space News/Space News/Features/Dashboard/Views/DashboardView.swift
+++ Space News/Space News/Features/Dashboard/Views/DashboardView.swift
@@ -2,7 +2,7 @@
 //  DashboardView.swift
 //  Space News
 //
-//  Created by Bambang Tri Rahmat Doni on 12/02/24.
+//  Created by Bambang Tri Rahmat Doni on 17/02/24.
 //
 
 import Inject
@@ -13,6 +13,8 @@ struct DashboardView: View {
   @State private var isShowingTabBar: Bool = true
 
   @Namespace private var animation
+
+  @Environment(\.colorScheme) private var colorScheme
 
   @ObservedObject private var injectObserver = Inject.observer
 
@@ -48,40 +50,40 @@ struct DashboardView: View {
                 .renderingMode(.template)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 20.0, height: 20.0)
-                .foregroundStyle(self.selectedTabItem == tab ? Color.white : Color.black)
-                .offset(y: self.selectedTabItem == tab ? -30.0 : 0)
-                .background {
+                .foregroundStyle(
+                  self.selectedTabItem == tab ?
+                    Color.white :
+                    Color.gray
+                )
+                .background(content: {
                   if self.selectedTabItem == tab {
-                    Circle()
-                      .fill(Color.black)
+                    RoundedRectangle(cornerRadius: 5.0)
+                      .fill(Color.appPrimary)
                       .scaleEffect(2.5)
-                      .shadow(color: Color.black.opacity(0.3), radius: 8.0, x: 5.0, y: 10.0)
+                      .shadow(color: Color.black.opacity(0.3), radius: 10.0, x: 5.0, y: 10.0)
                       .matchedGeometryEffect(id: "DashboardView", in: self.animation)
-                      .offset(y: self.selectedTabItem == tab ? -30.0 : 0)
                   }
-                }
+                })
                 .frame(maxWidth: .infinity)
-                .padding(.top, 15.0)
-                .padding(.bottom, 10.0)
+                .padding(.vertical, 20.0)
                 .contentShape(Rectangle())
             }
           }
         }
-        .padding(.horizontal)
         .offset(y: self.isShowingTabBar ? 0 : 150.0)
+        .background(content: {
+          AppTabViewCorner(corners: [.topLeft, .topRight], radius: 20.0)
+            .fill(Color.gray.opacity(0.3))
+            .ignoresSafeArea()
+        })
         .animation(
           .interactiveSpring(response: 0.5, dampingFraction: 0.65, blendDuration: 0.65),
           value: self.selectedTabItem
         )
         .animation(
-          .interactiveSpring(response: 0.6, dampingFraction: 0.7, blendDuration: 0.7),
+          .interactiveSpring(response: 0.5, dampingFraction: 0.65, blendDuration: 0.65),
           value: self.isShowingTabBar
         )
-        .background {
-          AppTabViewCorner(corners: [.topLeft, .topRight], radius: 25.0)
-            .fill(Color.gray)
-            .ignoresSafeArea()
-        }
       }
       .ignoresSafeArea(.keyboard, edges: .bottom)
       .onReceive(NotificationCenter.default.publisher(for: .init("SHOW_TAB_BAR"))) { _ in
@@ -90,7 +92,7 @@ struct DashboardView: View {
       .onReceive(NotificationCenter.default.publisher(for: .init("HIDE_TAB_BAR"))) { _ in
         self.isShowingTabBar = false
       }
-      .enableInjection()
     }
+    .enableInjection()
   }
 }
