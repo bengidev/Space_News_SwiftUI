ab1fb737586887471dd1b32a8bf5d22a30b0a58a
B e n g i <bambang.trd17@gmail.com>
2024-02-20 19:29:53 +0700

    refactor: remove unnecessary parameters

--- Space News/Space News/Utilities/AppPagination.swift
+++ Space News/Space News/Utilities/AppPagination.swift
@@ -8,110 +8,117 @@
 import SwiftUI
 
 struct AppPagination: View {
-    @Binding var currentPage: Int
-    let totalPages: Int
+  @Binding var currentPage: Int
+  let totalPages: Int
 
-    private let visiblePages = 5
-    private let leadingTrailingPages = 2
+  private let visiblePages = 5
+  private let leadingTrailingPages = 2
 
-    var body: some View {
-        HStack(spacing: 10) {
-            Button(action: {
-                if currentPage > 1 {
-                    currentPage -= 1
-                }
-            }) {
-                Image(systemName: "chevron.left").tint(Color.appPrimary)
-            }
-            .disabled(currentPage == 1)
-
-            if currentPage > 4 {
-                HStack {
-                    Button(action: {
-                        currentPage = 1
-                    }) {
-                        Text("\(1)")
-                            .font(.headline)
-                            .foregroundColor(Color.init(uiColor: .label))
-                            .frame(width: 30, height: 30)
-                            .background(Color.clear)
-                            .cornerRadius(15)
-                    }
-
-                    Text("...")
-                }
-            }
-
-            ForEach(pageNumbers(), id: \.self) { page in
-                if page <= 5 || page > totalPages - 5 || abs(page - currentPage) <= 2 {
-                    Button(action: {
-                        currentPage = page
-                    }) {
-                        Text("\(page)")
-                            .font(page == currentPage ? .headline : .subheadline)
-                            .foregroundColor(page == currentPage ? .appPrimary : Color.init(uiColor: .label))
-                            .frame(width: 30, height: 30)
-                            .background(page == currentPage ? Color.appPrimary.opacity(0.2) : Color.clear)
-                            .cornerRadius(15)
-                    }
-                }
-            }
-
-            if currentPage < totalPages - 2 {
-                HStack {
-                    if currentPage < totalPages - 3 {
-                        Text("...")
-                    }
-
-                    Button(action: {
-                        currentPage = totalPages
-                    }) {
-                        Text("\(totalPages)")
-                            .font(.headline)
-                            .foregroundColor(Color.init(uiColor: .label))
-                            .frame(width: 30, height: 30)
-                            .background(Color.clear)
-                            .cornerRadius(15)
-                    }
-                }
-            }
-
-            Button(action: {
-                print("Tester")
-                if currentPage < totalPages {
-                    currentPage += 1
-                }
-            }) {
-                Image(systemName: "chevron.right").tint(Color.appPrimary)
-            }
-            .disabled(currentPage == totalPages)
+  var body: some View {
+    HStack(spacing: 10) {
+      Button {
+        if self.currentPage > 1 {
+          self.currentPage -= 1
         }
-        .animation(.easeInOut, value: currentPage)
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .padding()
-        .background(Color.appSecondary)
+      } label: {
+        Image(systemName: "chevron.left").tint(Color.appPrimary)
+      }
+      .disabled(self.currentPage == 1)
+
+      if self.currentPage > 4 {
+        HStack {
+          Button {
+            self.currentPage = 1
+          } label: {
+            Text("\(1)")
+              .font(.headline)
+              .foregroundColor(Color(uiColor: .label))
+              .frame(width: 30, height: 30)
+              .background(Color.clear)
+              .cornerRadius(15)
+          }
+
+          Text("...")
+        }
+      }
+
+      ForEach(self.pageNumbers(), id: \.self) { page in
+        if page <= 5 || page > self.totalPages - 5 || abs(page - self.currentPage) <= 2 {
+          Button {
+            self.currentPage = page
+          } label: {
+            Text("\(page)")
+              .font(page == self.currentPage ? .headline : .subheadline)
+              .foregroundColor(page == self.currentPage ? .appPrimary : Color(uiColor: .label))
+              .frame(width: 30, height: 30)
+              .background(page == self.currentPage ? Color.appPrimary.opacity(0.2) : Color.clear)
+              .cornerRadius(15)
+          }
+        }
+      }
+
+      if self.currentPage < self.totalPages - 2 {
+        HStack {
+          if self.currentPage < self.totalPages - 3 {
+            Text("...")
+          }
+
+          Button {
+            self.currentPage = self.totalPages
+          } label: {
+            Text("\(self.totalPages)")
+              .font(.headline)
+              .foregroundColor(Color(uiColor: .label))
+              .frame(width: 30, height: 30)
+              .background(Color.clear)
+              .cornerRadius(15)
+          }
+        }
+      }
+
+      Button {
+        print("Tester")
+        if self.currentPage < self.totalPages {
+          self.currentPage += 1
+        }
+      } label: {
+        Image(systemName: "chevron.right").tint(Color.appPrimary)
+      }
+      .disabled(self.currentPage == self.totalPages)
+    }
+    .animation(.easeInOut, value: self.currentPage)
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .padding()
+    .background(Color.appSecondary)
+  }
+
+  private func pageNumbers() -> [Int] {
+    var numbers: [Int] = []
+
+    if self.totalPages <= self.visiblePages {
+      numbers = Array(1 ... self.totalPages)
+    } else {
+      // Display first 5 pages
+      if self.currentPage <= self.leadingTrailingPages + 1 {
+        numbers = Array(1 ... (self.visiblePages + self.leadingTrailingPages))
+      }
+      // Display last 5 pages
+      else if self.currentPage >= self.totalPages - self.leadingTrailingPages {
+        numbers = Array(
+          (self.totalPages - self.visiblePages - self.leadingTrailingPages + 1) ... self
+            .totalPages
+        )
+      }
+      // Display 5 pages around the current page
+      else {
+        numbers =
+          Array(
+            (self.currentPage - self.leadingTrailingPages) ...
+              (self.currentPage + self.leadingTrailingPages)
+          )
+      }
     }
 
-    private func pageNumbers() -> [Int] {
-        var numbers: [Int] = []
-
-        if totalPages <= visiblePages {
-            numbers = Array(1...totalPages)
-        } else {
-                // Display first 5 pages
-            if currentPage <= leadingTrailingPages + 1 {
-                numbers = Array(1...(visiblePages + leadingTrailingPages))
-            }
-                // Display last 5 pages
-            else if currentPage >= totalPages - leadingTrailingPages {
-                numbers = Array((totalPages - visiblePages - leadingTrailingPages + 1)...totalPages)
-            }
-                // Display 5 pages around the current page
-            else {
-                numbers = Array((currentPage - leadingTrailingPages)...(currentPage + leadingTrailingPages))
-            }
-        }
-
-        return numbers
-    }
+    return numbers
+  }
 }
