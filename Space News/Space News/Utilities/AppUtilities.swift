2e2a277a1e10021e782c50aa2738d03b6fea35e5
B e n g i <bambang.trd17@gmail.com>
2024-02-20 20:11:59 +0700

    chore: add AppUtilities into project Utilities

--- Space News/Space News/Utilities/AppUtilities.swift
+++ Space News/Space News/Utilities/AppUtilities.swift
@@ -5,4 +5,31 @@
 //  Created by Bambang Tri Rahmat Doni on 20/02/24.
 //
 
-import Foundation
+import SwiftUI
+
+final class AppUtilities {
+  @AppStorage("selectedAppearance") var selectedAppearance = 0
+
+  private var userInterfaceStyle: ColorScheme? = .dark
+
+  static let shared: AppUtilities = .init()
+
+  private init() {}
+
+  func overrideDisplayMode() {
+    let userInterfaceStyle: UIUserInterfaceStyle = if self.selectedAppearance == 2 {
+      .dark
+    } else if self.selectedAppearance == 1 {
+      .light
+    } else {
+      .unspecified
+    }
+
+    UIApplication.shared.currentUIWindow()?.overrideUserInterfaceStyle = userInterfaceStyle
+    if let window = UIApplication.shared.currentUIWindow() {
+      UIView.transition(with: window, duration: 0.3, options: .transitionCrossDissolve) {
+        window.overrideUserInterfaceStyle = userInterfaceStyle
+      }
+    }
+  }
+}
