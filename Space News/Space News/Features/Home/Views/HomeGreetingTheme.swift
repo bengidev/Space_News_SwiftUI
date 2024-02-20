2f9178d9583f3f61fc651658b25dce90538b2b1a
B e n g i <bambang.trd17@gmail.com>
2024-02-20 19:23:33 +0700

    chore: add SwiftFormat into XCode project

---
+++ Space News/Space News/Features/Home/Views/HomeGreetingTheme.swift
@@ -0,0 +1,18 @@
+//
+//  HomeGreetingTheme.swift
+//  Space News
+//
+//  Created by Bambang Tri Rahmat Doni on 20/02/24.
+//
+
+import SwiftUI
+
+struct HomeGreetingTheme: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeGreetingTheme()
+}
