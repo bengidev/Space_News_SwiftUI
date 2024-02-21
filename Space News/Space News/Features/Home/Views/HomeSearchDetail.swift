f4eabdbf1df085c880d98c861153ab0dcc9f0ecb
B e n g i <bambang.trd17@gmail.com>
2024-02-21 19:09:42 +0700

    chore: add HomeSearchDetail into Home features

---
+++ Space News/Space News/Features/Home/Views/HomeSearchDetail.swift
@@ -0,0 +1,14 @@
+//
+//  HomeSearchDetail.swift
+//  Space News
+//
+//  Created by Bambang Tri Rahmat Doni on 21/02/24.
+//
+
+import SwiftUI
+
+struct HomeSearchDetail: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
