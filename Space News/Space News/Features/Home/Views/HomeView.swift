1fe18e96c5072919e95c3b04aaf0c19add70aa9e
B e n g i <bambang.trd17@gmail.com>
2024-02-24 18:11:37 +0700

    refactor: separate view components to single view

--- Space News/Space News/Features/Home/Views/HomeView.swift
+++ Space News/Space News/Features/Home/Views/HomeView.swift
@@ -14,8 +14,9 @@ struct HomeView: View {
   @State private var searchText: String = ""
   @State private var tabs: [String] = ["Nature", "Animals", "Fish", "Flowers", "Cities", "Cars", "Planes"]
   @State private var selectedTab: Int = 0
+  @State private var carousels: [String] = ["Nature", "Animals", "Fish", "Flowers", "Cities", "Cars", "Planes"]
   @State private var selectedCarousel: Int = 0
-  @State private var showNewsDetail = false
+  @State private var isShowNewsDetail = false
   @State private var currentDetailNews: String = ""
   @State private var offset: CGFloat = 0
   @State private var lastOffset: CGFloat = 0
@@ -79,174 +80,118 @@ struct HomeView: View {
           }
           .padding(.top, 10.0)
 
-          Text("Trending News")
+          HomeTrendingNews(
+            carousels: self.carousels,
+            selectedCarousel: self.$selectedCarousel,
+            isShowNewsDetail: self.$isShowNewsDetail
+          )
+
+          Text("Highlights")
             .font(.system(.subheadline, design: .rounded).bold())
             .frame(maxWidth: .infinity, alignment: .leading)
             .padding(.horizontal, 5.0)
-            .padding(.vertical, 10.0)
+            .padding(.top, 5.0)
 
-          AppCardCarousel(
-            index: self.$selectedCarousel,
-            items: self.tabs,
-            spacing: 10.0,
-            cardPadding: 90.0,
-            id: \.self
-          ) { carousel, _ in
-            ZStack {
-              Image(systemName: "pencil")
-                .resizable()
-
-              VStack {
-                Text(
-                  "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit"
-                )
-                .font(.system(.subheadline, design: .serif))
-
-                HStack {
-                  Text("CNN International")
-                    .font(.system(.footnote, design: .rounded))
-                    .foregroundStyle(Color.gray)
-
-                  Spacer()
-
-                  Text("5 h")
-                    .font(.system(.footnote, design: .rounded))
-                    .foregroundStyle(Color.gray)
+          ForEach(0 ..< 10, id: \.self) { _ in
+            LazyVStack(spacing: 0) {
+              Button {
+                withAnimation(.interactiveSpring(
+                  response: 0.5,
+                  dampingFraction: 0.7,
+                  blendDuration: 0.7
+                )) {
+                  self.isShowNewsDetail = true
                 }
-                .padding(.vertical, 5.0)
               }
-              .padding()
-              .position(x: 140.0, y: 150.0)
-            }
-            .background(Color.gray.opacity(0.5))
-            .clipShape(RoundedRectangle(cornerRadius: 10.0))
-            .contentShape(Rectangle())
-            .onTapGesture {
-              withAnimation(.interactiveSpring(response: 0.5, dampingFraction: 0.7, blendDuration: 0.7)) {
-                self.currentDetailNews = carousel
-                self.showNewsDetail.toggle()
-              }
-            }
-          }
-          .padding(.horizontal, 10.0)
-          .frame(height: 200.0)
-        }
-        NavigationLink(
-          destination: HomeNewsDetail(),
-          isActive: self.$showNewsDetail,
-          label: {}
-        )
+              label: {
+                VStack(alignment: .center, spacing: 0) {
+                  Text(
+                    "Nequere porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velitNequere porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit"
+                  )
+                  .font(.system(.subheadline, design: .serif))
+                  .padding(5.0)
 
-        HStack {
-          ForEach(0 ..< self.tabs.count, id: \.self) { carousel in
-            RoundedRectangle(cornerRadius: 5.0)
-              .foregroundStyle(self.selectedCarousel == carousel ? Color.appPrimary : Color.gray.opacity(0.3))
-              .frame(width: self.selectedCarousel == carousel ? 20.0 : 10.0)
-          }
-        }
-        .padding(.top, 12.0)
-        .animation(
-          .interactiveSpring(response: 0.5, dampingFraction: 0.7, blendDuration: 0.7),
-          value: self.selectedCarousel
-        )
+                  Image(systemName: "pencil")
+                    .resizable()
+                    .frame(height: 120.0)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.red)
+                    .clipShape(RoundedRectangle(cornerRadius: 10.0))
 
-        Text("Highlights")
-          .font(.system(.subheadline, design: .rounded).bold())
-          .frame(maxWidth: .infinity, alignment: .leading)
-          .padding(.horizontal, 5.0)
-          .padding(.top, 5.0)
+                  HStack(spacing: 0) {
+                    Text("1h")
 
-        ForEach(0 ..< 10, id: \.self) { _ in
-          LazyVStack(spacing: 0) {
-            Button {
-              withAnimation(.interactiveSpring(response: 0.5, dampingFraction: 0.7, blendDuration: 0.7)) {
-                self.showNewsDetail = true
-              }
-            }
-            label: {
-              VStack(alignment: .center, spacing: 0) {
-                Text(
-                  "Nequere porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velitNequere porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit"
-                )
-                .font(.system(.subheadline, design: .serif))
-                .padding(5.0)
+                    Text("|")
+                      .padding(.horizontal, 5.0)
 
-                Image(systemName: "pencil")
-                  .resizable()
-                  .frame(height: 120.0)
-                  .frame(maxWidth: .infinity)
-                  .background(Color.red)
-                  .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                    Text("CNBC News")
 
-                HStack(spacing: 0) {
-                  Text("1h")
-
-                  Text("|")
-                    .padding(.horizontal, 5.0)
-
-                  Text("CNBC News")
-
-                  Spacer()
+                    Spacer()
+                  }
+                  .font(.system(.footnote, design: .rounded))
+                  .foregroundStyle(Color.gray)
+                  .padding(.vertical, 5.0)
+                  .padding(.horizontal, 10.0)
                 }
-                .font(.system(.footnote, design: .rounded))
-                .foregroundStyle(Color.gray)
-                .padding(.vertical, 5.0)
-                .padding(.horizontal, 10.0)
+                .frame(height: 200.0)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color.gray.opacity(0.3))
+                .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                .padding(.vertical, 3.0)
+                .padding(.horizontal, 5.0)
               }
-              .frame(height: 200.0)
-              .frame(maxWidth: .infinity, alignment: .leading)
-              .background(Color.gray.opacity(0.3))
-              .clipShape(RoundedRectangle(cornerRadius: 10.0))
-              .padding(.vertical, 3.0)
-              .padding(.horizontal, 5.0)
+              .buttonStyle(.plain)
             }
-            .buttonStyle(.plain)
+            .padding(.horizontal, 5.0)
           }
-          .padding(.horizontal, 5.0)
-        }
 
-        AppPagination(currentPage: self.$selectedPage, totalPages: 50)
-          .padding(.bottom, 60.0)
+          AppPagination(currentPage: self.$selectedPage, totalPages: 50)
+            .padding(.bottom, 60.0)
+        }
       }
-    }
-    .overlay {
-      GeometryReader { geometry -> Color in
-        let minY = geometry.frame(in: .named("HomeViewScroll")).minY
-        let limitOffset: CGFloat = 400.0
-        let durationOffset: CGFloat = 35.0
+      .overlay {
+        GeometryReader { geometry -> Color in
+          let minY = geometry.frame(in: .named("HomeViewScroll")).minY
+          let limitOffset: CGFloat = 400.0
+          let durationOffset: CGFloat = 35.0
 
-        DispatchQueue.main.async {
-          if minY < self.offset {
-            if self.offset < limitOffset, -minY > (self.lastOffset + durationOffset) {
+          DispatchQueue.main.async {
+            if minY < self.offset {
+              if self.offset < limitOffset, -minY > (self.lastOffset + durationOffset) {
+                withAnimation(.easeInOut.speed(1.5)) {
+                  NotificationCenter.default.post(
+                    name: .init("HIDE_TAB_BAR"),
+                    object: nil,
+                    userInfo: nil
+                  )
+                }
+
+                self.lastOffset = -self.offset
+              }
+            }
+
+            if minY > self.offset, -minY < (self.lastOffset - durationOffset) {
               withAnimation(.easeInOut.speed(1.5)) {
-                NotificationCenter.default.post(name: .init("HIDE_TAB_BAR"), object: nil, userInfo: nil)
+                NotificationCenter.default.post(name: .init("SHOW_TAB_BAR"), object: nil, userInfo: nil)
               }
 
               self.lastOffset = -self.offset
             }
+
+            self.offset = minY
           }
 
-          if minY > self.offset, -minY < (self.lastOffset - durationOffset) {
-            withAnimation(.easeInOut.speed(1.5)) {
-              NotificationCenter.default.post(name: .init("SHOW_TAB_BAR"), object: nil, userInfo: nil)
-            }
-
-            self.lastOffset = -self.offset
-          }
-
-          self.offset = minY
+          return Color.clear
         }
-
-        return Color.clear
       }
-    }
-    .clipped()
-    .background(Color.appSecondary)
-    .coordinateSpace(name: "HomeViewScroll")
-    .enableInjection()
-    .onAppear {
-      withAnimation(.easeInOut) {
-        self.greetingMessage = calculateGreetingTime().rawValue
+      .clipped()
+      .background(Color.appSecondary)
+      .coordinateSpace(name: "HomeViewScroll")
+      .enableInjection()
+      .onAppear {
+        withAnimation(.easeInOut) {
+          self.greetingMessage = calculateGreetingTime().rawValue
+        }
       }
     }
   }
