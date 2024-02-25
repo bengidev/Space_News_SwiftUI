bbdd7e1a9eec737ff16703e286eedd41f2b4ee82
B e n g i <bambang.trd17@gmail.com>
2024-02-25 20:58:43 +0700

    chore: add UICollectionView into Extensions

---
+++ Space News/Space News/Extensions/UICollectionView.swift
@@ -0,0 +1,30 @@
+//
+//  UICollectionView.swift
+//  Space News
+//
+//  Created by Bambang Tri Rahmat Doni on 25/02/24.
+//
+
+import SwiftUI
+
+extension UICollectionReusableView {
+  override open var backgroundColor: UIColor? {
+    get { .clear }
+    set {}
+
+    // default separators use same color as background
+    // so to have it same but new (say red) it can be
+    // used as below, otherwise we just need custom separators
+    //
+    // set { super.backgroundColor = .red }
+
+  }
+}
+
+extension View {
+  func listBackgroundColor(_ color: Color = .clear) -> some View {
+    self.onAppear {
+      UICollectionView.appearance().backgroundColor = UIColor(color)
+    }
+  }
+}
