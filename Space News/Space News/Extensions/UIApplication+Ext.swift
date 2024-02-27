0d68a3998e0bc0af6009876f6cd4d06bdea1ef46
B e n g i <bambang.trd17@gmail.com>
2024-02-27 22:19:26 +0700

    refactor: UIApplication into UIApplication+Ext

---
+++ Space News/Space News/Extensions/UIApplication+Ext.swift
@@ -0,0 +1,23 @@
+//
+//  UIApplication+Ext.swift
+//  Space News
+//
+//  Created by Bambang Tri Rahmat Doni on 20/02/24.
+//
+
+import SwiftUI
+
+public extension UIApplication {
+  func currentUIWindow() -> UIWindow? {
+    let connectedScenes = UIApplication.shared.connectedScenes
+      .filter { $0.activationState == .foregroundActive }
+      .compactMap { $0 as? UIWindowScene }
+
+    let window = connectedScenes.first?
+      .windows
+      .first { $0.isKeyWindow }
+
+    return window
+
+  }
+}
