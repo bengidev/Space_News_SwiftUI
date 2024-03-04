db60d56161661681e219bbf5d6537a220e0e700b
B e n g i <bambang.trd17@gmail.com>
2024-03-04 23:45:40 +0700

    refactor: remove unnecessary Extensions

--- Space News/Space News/Extensions/View+Ext.swift
+++ Space News/Space News/Extensions/View+Ext.swift
@@ -14,25 +14,3 @@ extension View {
     }
   }
 }
-
-@available(iOS 15.0, *)
-extension View {
-  func adaptiveSheet(
-    isPresented: Binding<Bool>,
-    detents: [UISheetPresentationController.Detent] = [.medium(), .large()],
-    smallestUndimmedDetentIdentifier: UISheetPresentationController.Detent.Identifier? = .medium,
-    prefersScrollingExpandsWhenScrolledToEdge: Bool = false,
-    prefersEdgeAttachedInCompactHeight: Bool = true,
-    @ViewBuilder content: @escaping () -> some View
-  ) -> some View {
-    return self
-      .modifier(AdaptiveSheet(
-        isPresented: isPresented,
-        detents: detents,
-        smallestUndimmedDetentIdentifier: smallestUndimmedDetentIdentifier,
-        prefersScrollingExpandsWhenScrolledToEdge: prefersScrollingExpandsWhenScrolledToEdge,
-        prefersEdgeAttachedInCompactHeight: prefersEdgeAttachedInCompactHeight,
-        content: content
-      ))
-  }
-}
