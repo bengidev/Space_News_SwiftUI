890a14d25cba486f3c4c4597876e3d3d80e309e5
B e n g i <bambang.trd17@gmail.com>
2024-03-08 00:49:18 +0700

    refactor: change required view components

--- Space News/Space News/Utilities/AppTagMenu.swift
+++ Space News/Space News/Utilities/AppTagMenu.swift
@@ -20,6 +20,31 @@ struct AppTagMenu: View {
       Text("Filter \nMenus")
         .font(.system(.title, design: .rounded).bold())
         .frame(maxWidth: .infinity, alignment: .leading)
+
+      TextField("Apple", text: self.$text)
+        .font(.title3)
+        .padding(.vertical, 12.0)
+        .padding(.horizontal)
+        .background {
+          RoundedRectangle(cornerRadius: 10.0, style: .continuous).strokeBorder(
+            Color.gray.opacity(0.5),
+            lineWidth: 1.0
+          )
+        }
+        .padding(.vertical, 12.0)
+
+      Button {
+      } label: {
+        Text("Add Tag")
+          .font(.system(.subheadline, design: .default).weight(.semibold))
+          .padding(.vertical, 12.0)
+          .padding(.horizontal, 45.0)
+          .background(Color.gray.opacity(0.3))
+          .clipShape(RoundedRectangle(cornerRadius: 10.0))
+      }
+      .buttonStyle(.plain)
+      .disabled(self.text.isEmpty)
+      .opacity(self.text.isEmpty ? 0.6 : 1.0)
     }
     .padding(15.0)
     .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
