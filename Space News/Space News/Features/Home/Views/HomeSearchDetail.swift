dd3ed77b7a30a7f879311ee10f727aaa6c0735fb
B e n g i <bambang.trd17@gmail.com>
2024-03-11 01:04:11 +0700

    refactor: change relevant view components

--- Space News/Space News/Features/Home/Views/HomeSearchDetail.swift
+++ Space News/Space News/Features/Home/Views/HomeSearchDetail.swift
@@ -6,7 +6,6 @@
 //
 
 import Inject
-import PartialSheet
 import SwiftUI
 
 struct HomeSearchDetail: View {
@@ -15,6 +14,13 @@ struct HomeSearchDetail: View {
   @State private var searchText: String = ""
   @State private var isShowFilter = false
   @State private var selectedDateRange: String = ""
+  @State private var tags: [Tag] = [
+    Tag(text: "Business", size: 64.75), Tag(text: "Politics", size: 51.9765625),
+    Tag(text: "Lifestyle", size: 60.7890625), Tag(text: "Art", size: 21.90625), Tag(text: "Health", size: 47.2421875),
+    Tag(text: "Tech", size: 34.8359375), Tag(text: "Travel", size: 43.25), Tag(text: "Fashion", size: 55.78125),
+    Tag(text: "Sports", size: 47.8203125), Tag(text: "Food", size: 36.640625), Tag(text: "World", size: 42.5625),
+    Tag(text: "Science", size: 57.484375)
+  ]
 
   @ObservedObject private var injectObserver = Inject.observer
 
@@ -29,128 +35,136 @@ struct HomeSearchDetail: View {
   private let dateRanges: [String] = ["today", "week", "month"]
 
   var body: some View {
-    VStack {
-      HStack {
+    ZStack {
+      VStack {
         HStack {
-          Image(systemName: "magnifyingglass")
+          HStack {
+            Image(systemName: "magnifyingglass")
 
-          TextField("Find interesting news", text: self.$searchText) { isEditing in
-            print("TextField isEditing: ", isEditing)
-          }
-          .font(.system(.body, design: .rounded))
-
-          Spacer()
-        }
-        .padding(10.0)
-        .overlay(
-          RoundedRectangle(cornerRadius: 10.0)
-            .stroke(Color.gray, lineWidth: 1.0)
-        )
-        .contentShape(Rectangle())
-
-        Button {
-          withAnimation { self.isShowFilter.toggle() }
-        } label: {
-          Image(systemName: "slider.horizontal.3")
-            .font(.title3)
-            .padding(10.0)
-            .background { RoundedRectangle(cornerRadius: 8.0).stroke(Color.gray, lineWidth: 1.0) }
-        }
-        .buttonStyle(.plain)
-        .contentShape(Rectangle())
-      }
-      .padding(.horizontal, 5.0)
-
-      ScrollView(.vertical, showsIndicators: false) {
-        ForEach(self.contacts, id: \.self) { _ in
-          LazyVStack {
-            Button {
-              withAnimation(.interactiveSpring(
-                response: 0.5,
-                dampingFraction: 0.7,
-                blendDuration: 0.7
-              )) {}
+            TextField("Find interesting news", text: self.$searchText) { isEditing in
+              print("TextField isEditing: ", isEditing)
             }
-            label: {
-              VStack(alignment: .center, spacing: 0) {
-                Text(
-                  "Nequere porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velitNequere porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit"
-                )
-                .font(.system(.subheadline, design: .serif))
-                .padding(5.0)
+            .font(.system(.body, design: .rounded))
 
-                Image(systemName: "pencil")
-                  .resizable()
-                  .frame(height: self.prop.size.height * 0.15)
-                  .frame(maxWidth: .infinity)
-                  .background(Color.red)
-                  .clipShape(RoundedRectangle(cornerRadius: 10.0))
+            Spacer()
+          }
+          .padding(10.0)
+          .overlay(
+            RoundedRectangle(cornerRadius: 10.0)
+              .stroke(Color.gray, lineWidth: 1.0)
+          )
+          .contentShape(Rectangle())
 
-                HStack(spacing: 0) {
-                  Text("1h")
+          Button {
+            withAnimation { self.isShowFilter.toggle() }
+          } label: {
+            Image(systemName: "slider.horizontal.3")
+              .font(.title3)
+              .padding(10.0)
+              .background { RoundedRectangle(cornerRadius: 8.0).stroke(Color.gray, lineWidth: 1.0) }
+          }
+          .buttonStyle(.plain)
+          .contentShape(Rectangle())
+        }
+        .padding(.horizontal, 5.0)
 
-                  Text("|")
-                    .padding(.horizontal, 5.0)
+        ScrollView(.vertical, showsIndicators: false) {
+          ForEach(self.contacts, id: \.self) { _ in
+            LazyVStack {
+              Button {
+                withAnimation(.interactiveSpring(
+                  response: 0.5,
+                  dampingFraction: 0.7,
+                  blendDuration: 0.7
+                )) {}
+              }
+              label: {
+                VStack(alignment: .center, spacing: 0) {
+                  Text(
+                    "Nequere porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velitNequere porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit"
+                  )
+                  .font(.system(.subheadline, design: .serif))
+                  .padding(5.0)
 
-                  Text("CNBC News")
+                  Image(systemName: "pencil")
+                    .resizable()
+                    .frame(height: self.prop.size.height * 0.15)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.red)
+                    .clipShape(RoundedRectangle(cornerRadius: 10.0))
 
-                  Spacer()
+                  HStack(spacing: 0) {
+                    Text("1h")
 
-                  Image(systemName: "star.fill")
+                    Text("|")
+                      .padding(.horizontal, 5.0)
+
+                    Text("CNBC News")
+
+                    Spacer()
+
+                    Image(systemName: "star.fill")
+                  }
+                  .font(.system(.footnote, design: .rounded))
+                  .foregroundStyle(Color.gray)
+                  .padding(.vertical, 5.0)
+                  .padding(.horizontal, 10.0)
                 }
-                .font(.system(.footnote, design: .rounded))
+                .frame(height: self.prop.size.height * 0.26)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color.gray.opacity(0.3))
+                .clipShape(RoundedRectangle(cornerRadius: 10.0))
+              }
+              .buttonStyle(.plain)
+              .contextMenu {
+                Button("Action 1") {}
+                Button("Action 2") {}
+                Button("Action 3") {}
+              }
+            }
+            .padding(.vertical, 5.0)
+            .padding(.horizontal, 10.0)
+          }
+        }
+        .onTapGesture {
+          self.isShowFilter = false
+        }
+      }
+
+      VStack {
+        Spacer()
+
+        VStack {
+          ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .leading) {
+              VStack {
+                Text("Filters")
+                  .font(.system(.headline, design: .rounded))
+              }
+              .frame(maxWidth: .infinity, alignment: .leading)
+              .background(Color.red)
+
+              Text(self.isShowFilter ? "Tester" : "Works only for news")
+                .font(.system(.subheadline, design: .default))
                 .foregroundStyle(Color.gray)
                 .padding(.vertical, 5.0)
-                .padding(.horizontal, 10.0)
-              }
-              .frame(height: self.prop.size.height * 0.26)
-              .frame(maxWidth: .infinity, alignment: .leading)
-              .background(Color.gray.opacity(0.3))
-              .clipShape(RoundedRectangle(cornerRadius: 10.0))
-            }
-            .buttonStyle(.plain)
-            .contextMenu {
-              Button("Action 1") {}
-              Button("Action 2") {}
-              Button("Action 3") {}
+
+              Text("Date Range")
+                .font(.system(.headline, design: .rounded))
+                .padding(.vertical, 5.0)
+
+              Text("Category (3)")
+                .font(.system(.headline, design: .rounded))
+                .padding(.vertical, 5.0)
+                .background(Color.red)
             }
           }
-          .padding(.vertical, 5.0)
-          .padding(.horizontal, 10.0)
         }
+        .frame(maxWidth: .infinity, maxHeight: self.prop.size.height * 0.3)
+        .background(Color.gray)
+        .clipShape(RoundedRectangle(cornerRadius: 25.0))
       }
-      .onTapGesture {
-        self.isShowFilter = false
-      }
-    }
-    .partialSheet(isPresented: self.$isShowFilter) {
-      ScrollView(.vertical, showsIndicators: false) {
-        VStack(alignment: .leading) {
-          VStack {
-            Text("Filters")
-              .font(.system(.headline, design: .rounded))
 
-            Text(self.isShowFilter ? "Tester" : "Works only for news")
-              .font(.system(.subheadline, design: .default))
-              .foregroundStyle(Color.gray)
-          }
-          .padding(.vertical, 5.0)
-
-          VStack {
-            Text("Date Range")
-              .font(.system(.headline, design: .rounded))
-          }
-          .padding(.vertical, 5.0)
-
-          VStack {
-            Text("Category (3)")
-              .font(.system(.headline, design: .rounded))
-          }
-          .padding(.vertical, 5.0)
-        }
-      }
-      .padding()
-      .frame(maxWidth: .infinity, maxHeight: self.prop.size.height * 0.5, alignment: .leading)
     }
     .animation(.easeInOut, value: self.isShowFilter)
     .animation(.easeInOut, value: self.selectedDateRange)
